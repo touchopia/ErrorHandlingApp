62234328572d75b265a90fe076e6a5f2866f95d6
Phil Wright <touchopia@me.com>
2022-06-14 18:06:21 -0600

    Create viewModel that observes with closure

--- ErrorHandlingApp/ViewController.swift
+++ ErrorHandlingApp/ViewController.swift
@@ -7,16 +7,52 @@
 
 import UIKit
 
+class MyViewModel {
+    var playerMessage: String? {
+        didSet {
+            playerMessageChanged?(playerMessage)
+        }
+    }
+    var playerMessageChanged: ((String?) -> Void)?
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var playerLabel: UILabel!
     @IBOutlet weak var errorMessageLabel: UILabel!
     
+    var viewModel = MyViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        errorMessageLabel.text = "The Error Message"
+        setupBindings()
+        
+        playerLabel.text = "Player 1"
     }
-
+    
+    private func setupBindings() {
+        viewModel.playerMessageChanged = { [weak self] newMessage in
+            self?.updateErrorMessage(with: newMessage)
+        }
+    }
+    
+    private func updateErrorMessage(with message: String?) {
+        errorMessageLabel?.text = message
+    }
+    
+    @IBAction func didTapRandomMessage(_ sender: UIButton) {
+        viewModel.playerMessage = "Random Player Message: \(createWord(maxSize: 15))"
+    }
+    
+    private func createWord(maxSize: Int) -> String {
+        var word = ""
+        for _ in 1...maxSize {
+            let randomChar = String(format: "%c", Int.random(in: 97..<123)) as String
+            word += randomChar
+        }
+        return word
+    }
+    
 }
 
